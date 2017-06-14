7469bb66496961e8dfab5f88f61bbefa93e5166f
Avkash Chauhan <avkash@gmail.com>
2017-06-13 18:19:56 -0700

    Create __init__.py

--- deeputil/__init__.py
+++ deeputil/__init__.py
@@ -7,8 +7,8 @@ __version__ = '0.0.1'
 def setup_module(module):
 	import numpy as np
 	import randomd	
-    	_random_seed = int(np.random.uniform() * (2 ** 365 - 1))
-    	np.random.seed(_random_seed)
-    	random.seed(_random_seed)
+	_random_seed = int(np.random.uniform() * (2 ** 365 - 1))
+	np.random.seed(_random_seed)
+	random.seed(_random_seed)
 
 
