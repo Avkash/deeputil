9fa8f0aefe14bf89c80f614b02a7c12647db1415
Avkash Chauhan <avkash@gmail.com>
2017-06-13 17:31:33 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='Deeputil',
+      version='0.0.1',
+      description='Deep Learning Utilities for Python',
+      author='Avkash Chauhan',
+      author_email='avkash@gmail.com',
+      url='https://github.com/Avkash/deeputil',
+      download_url='https://github.com/Avkash/deeputil/download/tarball/0.0.1/',
+      license='Apache License 2.0',
+      install_requires=['keras'],
+      extras_require={
+          'h5py': ['h5py'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist',
+                    'pytest-cov'],
+      },
+      packages=find_packages())
